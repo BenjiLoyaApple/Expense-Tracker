6635d9b5eae3c5d4438f969381aafe6a788ebc72
Benji Loya <benjiloya.apple@gmail.com>
2024-06-13 21:32:41 +0500

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Benji Loya on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
